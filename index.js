460a887268eabb491fd8adcdbb081e8b017751ad
Kerr Ivan Agapito Cirilo <kerr.ivan29@gmail.com>
2018-07-25 21:16:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 function add(x, y) {
   return x + y 
 }
-add( 4, 2)
 console.log(add(4,2))
+
+function subtract(x,y) {
+  return x - y 
+}
+console.log(subtract(60,40))
+
+function multiply() {
+  return x * y
+}
+console.log(multiply(2,3.4))
+
+cuntio
