c43d3f7765d77c0d3e6080f9c507c93e4a833617
Kerr Ivan Agapito Cirilo <kerr.ivan29@gmail.com>
2018-07-25 21:15:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,30 +1,5 @@
-var number = 10
-
-function add5() {
-  number += 5 
+function add(x, y) {
+  return x + y 
 }
-
-function divideBy3() {
-  number /= 3
-}
-
-divideBy3 ()
-
-console.log (number)
-
-add5()
-
-console.log( number )
-
-number = 10
-add5
-add5()
-console.log(number)
-divideBy3()
-console.log(number)
-
-function add(a,b) {
-  return a + b 
-}
-add(5,10)
-
+add( 4, 2)
+console.log(add(4,2))
